d8f66774588a5d398035d9e9f14156d17745f56e
unknown <juan.gomez.freelance@gmail.com>
2024-01-11 11:10:55 +0100

    first add constructor practica1

--- src/util/Cliente.java
+++ src/util/Cliente.java
@@ -3,5 +3,10 @@ package util;
 public class Cliente {
 	public int id;
 	public String nombre;
+	public Cliente(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
     
 }
